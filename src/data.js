110d764c2177faa95e2c3414ed31765912fd20a7
Carlos Fernández <sirikon2@gmail.com>
2018-08-09 00:32:33 +0200

    Computed data properties

--- src/data.js
+++ src/data.js
@@ -2,44 +2,57 @@ const fs = require('fs');
 const path = require('path');
 const { filenameWithoutExtension } = require('./utils');
 
-const DATA_DIR = './data';
-const DATAPATH_SYMBOL = Symbol('Data Path');
+const DATA_BASE_DIR = './data';
+const DATA_PATH_SYMBOL = Symbol('Data Path');
 
-function getDirectoryFiles(directoryPath) {
-    return fs.readdirSync(directoryPath);
-}
+const joinPath = (chunks) => path.join.apply(null, chunks);
+const getDirectoryFiles = (directoryPath) => fs.readdirSync(directoryPath);
+const fileIsDirectory = (filePath) => fs.existsSync(joinPath(filePath)) && fs.statSync(joinPath(filePath)).isDirectory();
 
 function getMatchingFileInDirectory(name, directory) {
     var matchingFiles = getDirectoryFiles(directory)
         .filter(f => filenameWithoutExtension(f) === name);
     
     if (matchingFiles.length > 0) {
-        return matchingFiles[0];
+        return path.join(directory, matchingFiles[0]);
     }
 
     return null;
 }
 
-function createDataProxy(dataPath) {
-    return new Proxy({[DATAPATH_SYMBOL]: dataPath || []}, {
-        has: () => true,
-        get(target, prop) {
-            if (typeof prop === 'string') {
-                
-                var filePath = path.join.apply(null, [DATA_DIR].concat(target[DATAPATH_SYMBOL], [prop]));
+function requireUncached(modulePath){
+    delete require.cache[require.resolve(modulePath)]
+    return require(modulePath)
+}
 
-                if(fs.existsSync(filePath) && fs.statSync(filePath).isDirectory()) {
-                    return createDataProxy(target[DATAPATH_SYMBOL].concat(prop));
+function createDataProxy(dataPath) {
+    let baseObject = {
+        [DATA_PATH_SYMBOL]: dataPath || []
+    };
+    return new Proxy(baseObject, {
+        has: () => true,
+        get(target, propName) {
+            if (typeof propName === 'string') {
+                const currentDataPath = target[DATA_PATH_SYMBOL];
+                const nextDataPath = currentDataPath.concat(propName);
+                
+                if(fileIsDirectory(nextDataPath)) {
+                    return createDataProxy(currentDataPath.concat(propName));
                 } else {
-                    var directory = path.dirname(filePath);
-                    var matchingFile = getMatchingFileInDirectory(prop, directory);
+                    var directory = joinPath([DATA_BASE_DIR].concat(currentDataPath));
+                    var matchingFile = getMatchingFileInDirectory(propName, directory);
 
                     if (!matchingFile) {
                         throw new Error('Data not found:' + filePath);
                     }
 
-                    var content = fs.readFileSync(path.join(directory, matchingFile), { encoding: 'utf8' });
                     var extension = path.extname(matchingFile);
+                    
+                    if (extension === '.js') {
+                        return requireUncached(path.resolve(matchingFile)).data;
+                    }
+
+                    var content = fs.readFileSync(matchingFile, { encoding: 'utf8' });
 
                     if (extension === '.json') {
                         return JSON.parse(content);
