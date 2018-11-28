b9b23657ba67e7efa8cb06a63e194be28e25c7a4
Carlos Fernández Llamas <hello@sirikon.me>
2018-11-28 02:28:28 +0100

    Replace showdown for markdown parsing with remark (frontmatter, html, parse) and js-yaml.

--- src/data-readers/markdown-reader.js
+++ src/data-readers/markdown-reader.js
@@ -1,15 +1,38 @@
 const fs = require('fs');
-const showdown = require('showdown');
+const yaml = require('js-yaml');
+const unified = require('unified');
+const markdown = require('remark-parse');
+const frontmatter = require('remark-frontmatter');
+const html = require('remark-html');
+
+const markdownProcessor = unified()
+    .use(markdown)
+    .use(frontmatter, ['yaml'])
+    .use(html);
+
+function parseMarkdown(content) {
+    let meta = {};
+
+    const ast = markdownProcessor.runSync(markdownProcessor.parse(content));
+    
+    if (ast.children.length > 0 && ast.children[0].type === 'yaml') {
+        meta = yaml.safeLoad(ast.children[0].value);
+    }
+
+    const html = markdownProcessor.stringify(ast).trim();
+
+    return { meta, html };
+}
 
 module.exports = {
     fileExtension: '.md',
     read(filePath) {
-        const showdownConverter = new showdown.Converter({ metadata: true });
         const content = fs.readFileSync(filePath, { encoding: 'utf8' });
+        const document = parseMarkdown(content);
         return {
             text: content,
-            html: showdownConverter.makeHtml(content),
-            meta: showdownConverter.getMetadata()
+            html: document.html,
+            meta: document.meta
         };
     }
 };
